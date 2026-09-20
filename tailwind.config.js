efcf7de2d7831fd2eaa0ef8561bfa3dd6dc0ded4
crzamodafeminina-prog <crzamodafeminina@gmail.com>
2026-09-20 15:03:15 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        areia: "#E7DAC7",
+        areiaesc: "#D6C4AB",
+        offwhite: "#F8F5F0",
+        terra: "#AF4E2F",
+        terraesc: "#8E3C22",
+        cacau: "#3B2A21",
+        oliva: "#7A7A5E",
+      },
+      fontFamily: {
+        display: ["Fraunces", "Georgia", "serif"],
+        sans: ["Karla", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
